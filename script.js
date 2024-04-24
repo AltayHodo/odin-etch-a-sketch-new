b0e1a81cc0a78ab413b8f0400376e366b7b8cb7e
Altay Hodoglugil <altay.hodo@gmail.com>
2024-04-24 15:50:26 -0700

    Add updateGrid feature
    Added a button that enables the user to be able to
    change the dimensions of the grid, giving it more
    or less squares depending on the input, but
    keeping the grid the same total size

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
   createGrid(5);
-  addEventListeners();
 });
 
 function createGrid(n) {
   const gridContainer = document.querySelector('.grid-container');
+  gridContainer.innerHTML = '';
   gridContainer.style.display = 'grid';
   gridContainer.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
   for (let i = 0; i < n * n; i++) {
@@ -13,8 +13,8 @@ function createGrid(n) {
     gridItem.style.border = '1px solid';
     gridContainer.appendChild(gridItem);
   }
+  addEventListeners();
 }
-
 
 function addEventListeners(){
   const gridItems = document.querySelectorAll('.grid-item');
@@ -26,3 +26,13 @@ function addEventListeners(){
   });
 }
 
+const gridButton = document.querySelector('.grid-btn');
+gridButton.addEventListener('click', updateGridSize);
+
+function updateGridSize(){
+  let newSize = parseInt(prompt('Size of grid', 5));
+  while (newSize > 100 || newSize < 1 || isNaN(newSize)){
+    newSize = parseInt(prompt('Enter a number between 1 and 100', 5));
+  }
+  createGrid(newSize);
+}
