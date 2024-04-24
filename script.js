c1ce5ce67b3df05c4483a1332ad4590bdb61b8be
Altay Hodoglugil <altay.hodo@gmail.com>
2024-04-24 15:35:06 -0700

    Add hover affect functionality

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const gridContainer = document.querySelector('.grid-container');
-
+window.addEventListener('load', () => {
+  createGrid(5);
+  addEventListeners();
+});
 
 function createGrid(n) {
+  const gridContainer = document.querySelector('.grid-container');
   gridContainer.style.display = 'grid';
   gridContainer.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
   for (let i = 0; i < n * n; i++) {
     const gridItem = document.createElement('div');
-    gridItem.textContent = i;
-    gridItem.style.border = '1px solid'
+    gridItem.classList.add('grid-item');
+    gridItem.style.border = '1px solid';
     gridContainer.appendChild(gridItem);
   }
 }
 
-createGrid(5)
 
+function addEventListeners(){
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(gridItem => {
+    gridItem.addEventListener('mouseover', (e) =>{
+      grid = e.target;
+      grid.style.backgroundColor = 'red';
+    });
+  });
+}
 
